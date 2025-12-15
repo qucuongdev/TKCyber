e8c2f71a121d6febbc72069be685ee5c27fa695d
qucuongdev <qucuongdev@gmail.com>
2025-12-15 20:51:45 +0700

    update readme file

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#818cf8',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/logo7.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/logo7.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   };
